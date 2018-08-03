ef0101d103642749a5ef5a7bd50b434a549df658
geraldokutar <geraldpilk@gmail.com>
2018-08-03 12:01:09 +0300

    Add new contact to hold new names

--- js/script.js
+++ js/script.js
@@ -11,5 +11,8 @@ $(document).ready(function(){
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
 
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    
+
   })
 })

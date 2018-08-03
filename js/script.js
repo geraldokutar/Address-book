6afe133086afb1ae688a36c8943f9228b61eb120
geraldokutar <geraldpilk@gmail.com>
2018-08-03 16:00:07 +0300

    Add js rule so that first and second name show under contacts

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 // user interface logic
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
@@ -12,8 +15,8 @@ $(document).ready(function(){
     var inputtedLastName = $("input#new-last-name").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-    
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
     $(".contact").list().click(function(){
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);

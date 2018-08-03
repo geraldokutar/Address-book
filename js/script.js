f29d079c9528ba36298a57465ca4b034091d6ff1
geraldokutar <geraldpilk@gmail.com>
2018-08-03 11:48:17 +0300

    Add a js rule to form id new-contact

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+//business logic
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+// user interface logic
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+  })
+})
